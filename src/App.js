1f8883827fcae1d0969b7f07de476e5f35fb9a09
Maria Gromysch <mariaviktoria@protonmail.com>
2021-08-30 14:29:39 -0700

    my react  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ render() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p>Maria's Site</p>
+	<p>Story of Cheburashka - <a href=""></a>.</p>
+	<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
