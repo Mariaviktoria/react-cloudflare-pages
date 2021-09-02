27e46e36edeb184882125f229d2d762e46d072d2
Mariaviktoria <35540599+Mariaviktoria@users.noreply.github.com>
2021-09-02 14:56:56 -0700

    Update App.js
    Updating name tag

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ render() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Maria's Site</p>
+        <p>Maria Gromysh Site</p>
 	<p>Story of Cheburashka - <a href=""></a>.</p>
 	<p>
           Edit <code>src/App.js</code> and save to reload.
